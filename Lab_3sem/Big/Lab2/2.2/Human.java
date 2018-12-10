570744f135dc7119913d9030e6affe14d0be33a4
Shvedkov Ivan <32375298+Shveed@users.noreply.github.com>
2018-12-10 19:22:24 +0300

    Create Human.java

---
+++ Lab_3sem/Big/Lab2/2.2/Human.java
@@ -0,0 +1,17 @@
+package com.company;
+
+
+public class Human {
+    private String name;
+    private int age;
+
+    public Human(String Name, int Age){
+        name = Name;
+        age = Age;
+    }
+
+    public void Greeting(){
+        System.out.println("Hello, dear " + name);
+        System.out.println("We know, you are " + age + " years old");
+    }
+}
